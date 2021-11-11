f867776bd66db981e251493f2c337bcf6ac19754
prasad123456 <prasad.tech@gmail.com>
2021-11-11 10:50:26 +0530

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace gitdemo2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello its me");        
         }
+
     }
 }
